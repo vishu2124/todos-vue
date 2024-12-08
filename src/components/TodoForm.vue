1bb16c42aa0bc733755b2ca6ec508fbb0a29552b
Vishu Singh <schumi.offi2124@gmail.com>
2024-12-09 00:15:54 +0530

    [vishu] - todo forms setup

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <input v-model="title" placeholder="Enter task" required />
     <select v-model="priority" required>
       <option value="">-Priority-</option>
@@ -13,7 +13,75 @@
       <option value="Jack">Jack</option>
       <option value="Justin">Justin</option>
     </select>
-    <button type="submit">Add</button>
-    <button type="button">Cancel</button>
+    <button type="submit">{{ isEditing ? 'Update' : 'Add' }}</button>
+    <button type="button" v-if="isEditing" @click="cancelEdit">Cancel</button>
   </form>
 </template>
+
+<script lang="ts">
+import { ref, watch } from 'vue'
+
+export default {
+  props: {
+    todoToEdit: {
+      type: Object,
+      default: null,
+    },
+  },
+  emits: ['addTodo', 'updateTodo', 'cancelEdit'],
+  setup(props, { emit }) {
+    const title = ref('')
+    const priority = ref('optional')
+    const assignee = ref('')
+    const isEditing = ref(false)
+
+    watch(
+      () => props.todoToEdit,
+      (newTodo) => {
+        if (newTodo) {
+          isEditing.value = true
+          title.value = newTodo.title
+          priority.value = newTodo.priority
+          assignee.value = newTodo.assignee
+        }
+      },
+      { immediate: true },
+    )
+
+    const resetForm = () => {
+      title.value = ''
+      priority.value = ''
+      assignee.value = ''
+      isEditing.value = false
+    }
+    const cancelEdit = () => {
+      resetForm()
+      emit('cancelEdit')
+    }
+
+    const handleSubmit = () => {
+      const todo = {
+        title: title.value,
+        priority: priority.value,
+        assignee: assignee.value,
+      }
+      if (isEditing.value) {
+        emit('updateTodo', { ...props.todoToEdit, ...todo }) // Emit updated todo
+      } else {
+        todo.id = Date.now().toString()
+        emit('addTodo', todo) // Emit new todo
+      }
+      resetForm()
+    }
+
+    return {
+      isEditing,
+      title,
+      priority,
+      assignee,
+      handleSubmit,
+      cancelEdit,
+    }
+  },
+}
+</script>
