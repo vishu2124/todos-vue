58afaa8f1438dcc56bfc6e7a0107965952f212a6
Vishu Singh <schumi.offi2124@gmail.com>
2024-12-08 23:45:24 +0530

    [vishu] - addeded html

---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <ul>
+    <li>
+      Todos
+      <button>Sort</button>
+      <button>Clear All</button>
+    </li>
+    <li v-if="todos.length === 0">Nothing in the list</li>
+    <li v-for="todo in sortedTodos" :key="todo.id"></li>
+    <li></li>
+  </ul>
+</template>
