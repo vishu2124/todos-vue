6f6efa14d57dc077ef5a68a4e628fc6cf5f0d7b9
Vishu Singh <schumi.offi2124@gmail.com>
2024-12-08 23:47:17 +0530

    [vishu] - added  html

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form>
+    <input v-model="title" placeholder="Enter task" required />
+    <select v-model="priority" required>
+      <option value="">-Priority-</option>
+      <option value="critical">Critical</option>
+      <option value="moderate">Moderate</option>
+      <option value="optional">Optional</option>
+    </select>
+    <select v-model="assignee" required>
+      <option value="">-Assignee-</option>
+      <option value="John">John</option>
+      <option value="Jack">Jack</option>
+      <option value="Justin">Justin</option>
+    </select>
+    <button type="submit">Add</button>
+    <button type="button">Cancel</button>
+  </form>
+</template>
