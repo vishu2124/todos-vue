9b9acc0b3bfca5e5d1e4904579fc88808e59b75b
Vishu Singh <schumi.offi2124@gmail.com>
2024-12-08 23:37:12 +0530

    [vishu] - added required functions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import TodoForm from './components/TodoForm.vue'
 import TodoList from './components/TodoList.vue'
+
+const addTodo = () => {
+  // add todo
+}
+const deleteTodo = () => {
+  // deleted todo
+}
+
+const updateTodo = () => {
+  // update todo
+}
+
+const editTodo = () => {
+  // editTodo todo
+}
+const cancelEdit = () => {
+  // cancelEdit todo
+}
+
+const clearAll = () => {
+  // clear all todos
+}
 </script>
 <template>
   <header>
     <div class="wrapper"><h1>Todo</h1></div>
   </header>
-  <TodoForm />
-  <TodoList />
+  <TodoForm @addTodo="addTodo" @updateTodo="updateTodo" @cancelEdit="cancelEdit" />
+  <TodoList :todos="todos" @editTodo="editTodo" @deleteTodo="deleteTodo" @clearAll="clearAll" />
 </template>
+<style scoped></style>
