3788c6b5ab3c55d3da0dc830ef6dfe0048744525
Vishu Singh <schumi.offi2124@gmail.com>
2024-12-08 23:32:55 +0530

    [vishu] - added files and css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
+<script setup lang="ts">
+import TodoForm from './components/TodoForm.vue'
+import TodoList from './components/TodoList.vue'
+</script>
 <template>
   <header>
-    <div class="wrapper">
-     Hello world
-    </div>
+    <div class="wrapper"><h1>Todo</h1></div>
   </header>
+  <TodoForm />
+  <TodoList />
 </template>
-
